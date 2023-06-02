792b7b7bab9c69774b0510c0a24bef889fd28be1
Ayomide <ilesanmibry@gmail.com>
2023-06-02 03:09:18 +0100

    feat: Custom Api error

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,20 @@
 import User from "../models/userModel.js"
 import { StatusCodes } from 'http-status-codes';
 
+class CustomApiError extends Error {
+  constructor(message) {
+    super(message)
+    this.StatusCode = StatusCodes.BAD_REQUEST
+  }
+}
+
 
 const register = async (req, res, next) => {
 
   const {name, email, password} = req.body
 
   if(!name, !email, !password) {
-    throw new Error('Please provide all values')
+    throw new CustomApiError('Please provide all values')
   }
 
   const userr = await User.create(req.body);
